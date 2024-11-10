e94c329ea9d0f3c9d9c6cfa455abb7e23b2910ee
Noah Florence <104790587+njf62909@users.noreply.github.com>
2024-11-10 00:47:20 -0500

    fixing a nesting mistake i made

--- src/app/components/Search.tsx
+++
@@ -1,9 +0,0 @@
-import style from '../css/Items.module.css';
-
-export default function Search() {
-    return (
-        <>
-            <button className={style.button1}>Search</button>
-        </>
-    );
-};
