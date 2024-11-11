b34746c37be9e4a2e62c9e160650f1cf96134610
emmanuel-sp <esp48321@uga.edu>
2024-11-11 17:09:14 -0500

    Updated Login Page: Improved CSS styling and created Form

--- uga-lost-and-found/src/app/login/page.tsx
+++ uga-lost-and-found/src/app/login/page.tsx
@@ -2,10 +2,12 @@
 
 import { useAuth } from '../context/AuthContext';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import style from '../css/Login.module.css'
 
 export default function Login() {
-  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const { setLoggedIn } = useAuth();
   const router = useRouter();
 
@@ -18,11 +20,35 @@ export default function Login() {
     router.push('/items');
   };
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+  
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
   return (
     <div className={style.login}>
-      <h2 className={style.loginPrompt}>Welcome to the Login Page!</h2>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={cancelLogin}>Continue without logging in</button>
+      <h2 className={style.title}>Welcome Back!</h2>
+      <form className={style.form}>
+        <input className={style.input}
+          type="text" 
+          placeholder="Username" 
+          onChange={handleUsernameChange} 
+          value={username}
+        />
+        <input className={style.input}
+          type="text" 
+          placeholder="Password" 
+          onChange={handlePasswordChange} 
+          value={password}
+        />
+        <button type="submit" onClick={handleLogin}>Login</button>
+      </form>
+      <button className={style.signup}>Don't have an account? Sign Up!</button>
+      <button className={style.cancel} onClick={cancelLogin}>Continue without logging in</button>
+      
     </div>
   );
   
