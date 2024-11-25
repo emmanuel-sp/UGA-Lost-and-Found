80404abab9d1e577170392b9f4b014b3a2bbeae4
Nicholas McCullough <ncm19783@uga.edu>
2024-11-25 17:19:24 -0500

    added UserSchema.ts and user routing files

---
+++ uga-lost-and-found/src/app/api/users/route.ts
@@ -0,0 +1,53 @@
+import connectMongoDB from "@/libs/mongodb";
+import { User } from "@/models/UserSchema";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import bcrypt from 'bcrypt';
+
+export async function POST(request: NextRequest) {
+
+    try {
+        const { email, password } = await request.json();
+        await connectMongoDB();
+
+        /*
+        if (!email || !password) {
+            return NextResponse.json(
+                { message: "Email and password are required." },
+                { status: 400 }
+            );
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json(
+                { message: "User already exists." },
+                { status: 409 }
+            );
+        }*/
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+        });
+
+        await newUser.save();
+
+        return NextResponse.json(
+            {
+                message: "User created successfully.",
+                user: { email: newUser.email },
+            },
+            { status: 201 }
+        );
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return NextResponse.json(
+            { message: "Internal server error." },
+            { status: 500 }
+        );
+    }
+    
+}
---
+++ uga-lost-and-found/src/models/UserSchema.ts
@@ -0,0 +1,14 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+    _id: mongoose.Types.ObjectId;
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<IUser>({
+    email:{ type: String, required: true },
+    password:{ type: String, required: true }
+});
+
+export const User = mongoose.models.User ?? mongoose.model("User", userSchema)
