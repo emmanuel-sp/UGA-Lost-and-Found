1bd6275aebea94cb3ab43fce291fb81fe5c1d549
Nicholas McCullough <ncm19783@uga.edu>
2024-11-25 17:21:11 -0500

    added auth.ts and auth.config.ts files

---
+++ uga-lost-and-found/src/auth.ts
@@ -0,0 +1,52 @@
+import { authConfig } from "./auth.config";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { User, IUser } from "./models/UserSchema";
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    ...authConfig,
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                if (!credentials) return null;
+
+                try {
+                    const user = await User.findOne({ email: credentials.email }).lean<IUser | null>();
+                    if (user) {
+                        const isMatch = await bcrypt.compare(
+                            credentials.password as string,
+                            user.password
+                        );
+
+                        if (isMatch) {
+                            return {
+                                id: user._id.toString(),
+                                email: user.email,
+                                //name: user.username,
+                            };
+                        } else {
+                            console.log("Email or Password is not correct");
+                            return null;
+                        }
+                    } else {
+                        console.log("User not found");
+                        return null;
+                    }
+                } catch (error: any) {
+                    console.log("An error occured: ",error);
+                    return null;
+                }
+            },
+        }),
+    ],
+});
